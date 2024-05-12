b899c3dde03a56e2ca1c3bd912d159cc817b41ea
candidtim <timcandid@gmail.com>
2024-05-12 10:55:13 +0200

    Initial steps for change detection

--- life.lisp
+++ life.lisp
@@ -9,8 +9,8 @@
   (make-array (list height width) :element-type '(unsigned-byte 8) :initial-element 0))
 
 (defun inject (field figure at-i at-j &optional center-p)
-  (let ((loc-i (if center-p (- at-i (floor (array-dimension figure 0) 2)) (at-i)))
-        (loc-j (if center-p (- at-j (floor (array-dimension figure 1) 2)) (at-j))))
+  (let ((loc-i (if center-p (- at-i (floor (array-dimension figure 0) 2)) at-i))
+        (loc-j (if center-p (- at-j (floor (array-dimension figure 1) 2)) at-j)))
     (loop for i below (array-dimension figure 0) do
           (loop for j below (array-dimension figure 1) do
                 (setf (aref field (+ i loc-i) (+ j loc-j)) (aref figure i j))))))
@@ -41,32 +41,72 @@
 ;;; Gameplay
 
 (defun count-neighbors (field at-i at-j)
+  "Returns a number of alive cells in a 3x3 square aroud the given cell, itself incuded"
   (let ((box-top (max (1- at-i) 0))
         (box-bottom (min (1+ at-i) (1- (array-dimension field 0))))
         (box-left (max (1- at-j) 0))
         (box-right (min (1+ at-j) (1- (array-dimension field 1)))))
-    (loop for i from box-top to box-bottom sum
-          (loop for j from box-left to box-right count
-                (alivep (aref field i j))))))
+    (loop for i from box-top to box-bottom
+          sum (loop for j from box-left to box-right
+                    count (alivep (aref field i j))))))
+
+; TODO: represent newborn cells differently
+(defun evolve-cell (field i j)
+  "Returns next generation state for the given cell"
+  (case (count-neighbors field i j)
+    (3 1)
+    (4 (aref field i j))
+    (otherwise 0)))
+
+#+nil
+(progn
+  (defvar *test-field* (make-field 3 4))
+  (defun test-field-reset ()
+    (inject *test-field*
+            (parse-figure '("  * "
+                            "****"
+                            "   *"))
+            0 0 nil))
+  (test-field-reset)
+  (show-field *test-field*))
+
+#+nil
+(progn
+  (assert (= (evolve-cell *test-field* 0 0) 0))  ; dead
+  (assert (= (evolve-cell *test-field* 0 2) 1))  ; survided
+  (assert (= (evolve-cell *test-field* 0 3) 1))  ; born
+  (assert (= (evolve-cell *test-field* 1 0) 0))  ; died (lone)
+  (assert (= (evolve-cell *test-field* 1 2) 0))) ; died (overpopulation)
 
 (defun compute-next-gen (field)
-  (let ((new-field (make-field (array-dimension field 0) (array-dimension field 1))))
-    (loop for i below (array-dimension field 0) do
-          (loop for j below (array-dimension field 1) do
-                (case (count-neighbors field i j)
-                  (3 (setf (aref new-field i j) 1))
-                  (4 (setf (aref new-field i j) (aref field i j)))
-                  (otherwise (setf (aref new-field i j) 0)))))
+  (let* ((height (array-dimension field 0))
+         (width (array-dimension field 1))
+         (new-field (make-field height width)))
+    (loop for i below height
+          do (loop for j below width
+                   do (setf (aref new-field i j) (evolve-cell field i j))))
     new-field))
+
+#+nil
+(progn
+  (show-field *test-field*)
+  (show-field (compute-next-gen *test-field*)))
 
 (defun tick (field)
   "Evolve the field to the next generation. Returns the new population count."
-  (let ((next-gen (compute-next-gen field)))
-    (loop for i below (array-dimension field 0) sum
-          (loop for j below (array-dimension field 1)
-                count (alivep (aref next-gen i j))
-                do
-                (setf (aref field i j) (aref next-gen i j))))))
+  (let ((next-gen (compute-next-gen field))
+        (population 0)
+        (change-count 0))
+    (loop for i below (array-dimension field 0)
+          do (loop for j below (array-dimension field 1)
+                   do (if (alivep (aref next-gen i j)) (incf population))
+                      (setf (aref field i j) (aref next-gen i j))))))
+#+nil
+(progn
+  (show-field *test-field*)
+  (tick *test-field*)
+  (show-field *test-field*)
+  (test-field-reset))
 
 ; TODO: accept figures as an argument
 ; TODO: print from a separate thread?
